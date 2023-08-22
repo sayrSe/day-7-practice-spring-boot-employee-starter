7da1408c8d7099379f248cc60166e10a8b9d193c
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-22 17:01:38 +0800

    feat: add ResponseStatus in addEmployee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +29,7 @@ public class EmployeeController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody Employee employee){
         employeeRepository.addEmployee(employee);
         return employee;
