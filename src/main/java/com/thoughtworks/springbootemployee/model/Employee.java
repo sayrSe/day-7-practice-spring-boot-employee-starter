33f90ebe6c1952b70cb5d812282980931a2f8da7
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-23 15:52:30 +0800

    feat: implement create in EmployeeService should_return_create_employee_when_create_given_employee_service_and_employee_with_valid_age

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -21,6 +21,24 @@ public class Employee {
         this.salary = salary;
         this.companyId = companyId;
     }
+
+    public Employee(Long id, String name, Integer age, String gender, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+    }
+
+    public Employee(String name, Integer age, String gender, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+    }
+
     public Employee(String name, Integer age, String gender, Integer salary, Long companyId) {
         this.name = name;
         this.age = age;
---
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class EmployeeServiceTest {
+
+    private EmployeeService employeeService;
+    private EmployeeRepository mockedEmployeeRepository;
+
+    @BeforeEach
+    void setUp() {
+        mockedEmployeeRepository = mock(EmployeeRepository.class);
+        employeeService = new EmployeeService(mockedEmployeeRepository);
+    }
+
+    @Test
+    void should_return_create_employee_when_create_given_employee_service_and_employee_with_valid_age() {
+        // Given
+        Employee employee = new Employee("Lucy", 20, "Female", 3000);
+        Employee savedEmployee = new Employee(1L, "Lucy", 20, "Female", 3000);
+        when(mockedEmployeeRepository.addEmployee(employee)).thenReturn(savedEmployee);
+
+        // When
+        Employee employeeResponse = employeeService.create(employee);
+
+        // Then
+        assertEquals(savedEmployee.getId(), employeeResponse.getId());
+        assertEquals("Lucy", employeeResponse.getName());
+        assertEquals(20, employeeResponse.getAge());
+        assertEquals("Female", employeeResponse.getGender());
+        assertEquals(3000, employeeResponse.getSalary());
+    }
+}
